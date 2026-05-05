a1757a87f13b0ab5a20716952202a7526546acdc
Bryan España <esp21550@uvg.edu.gt>
2026-05-04 20:12:45 -0600

    Add user login page and integrate authentication with chat

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-  belongs_to :sender, class_name: 'User'
+  belongs_to :sender, class_name: 'User', optional: true
   belongs_to :recipient, class_name: 'User', optional: true
   
   # group_id points to a Group model that will be created later.
